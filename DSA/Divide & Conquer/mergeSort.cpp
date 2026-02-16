cd07c48081411f02b0c8cc1885657a057b0fffcd
Divyam Agarwal <divyam7246@gmail.com>
2026-02-16 17:03:45 +0530

    changes

--- DSA/Divide & Conquer/mergeSort.cpp
+++ DSA/Divide & Conquer/mergeSort.cpp
@@ -1,8 +1,40 @@
 #include<iostream>
+#include<vector>
 using namespace std ; 
+
+void merge(int arr[], int si, int mid, int ei){ // O(n)
+    vector<int> temp ;
+    int i = si;
+    int j = mid+1 ;
+    while(i <= mid && j <= ei){
+        if(arr[i]<arr[j]) temp.push_back(arr[i++]);
+        else temp.push_back(arr[j++]);
+    }
+    while(i <= mid ) temp.push_back(arr[i++]);
+    while( j <= ei ) temp.push_back(arr[j++]);
+    // vector -> original array 
+    for(int i=si, x = 0 ; i <= ei; i++ ) arr[i] = temp[x++]; 
+}
+
+void mergeSort(int arr[], int si, int ei){ // O(nlogn)
+    if(si>=ei) return ;
+
+    int mid = si + ( ei - si ) / 2 ;
+    mergeSort(arr,si,mid); // Left Half
+    mergeSort(arr,mid+1,ei); // Right Half
+    merge(arr,si,mid,ei); // conquer 
+}
+
+void printArray(int arr[], int n){
+    for(int i=0; i <n ; i++ ) cout << arr[i] << " " ;
+    cout << endl;
+}
 
 int main()
 {
-    
+    int arr[6] = {6,3,7,5,2,4};
+    int n = sizeof(arr)/sizeof(arr[0]) ;
+    mergeSort(arr,0,n-1);
+    printArray(arr,n);
     return 0 ; 
 }
