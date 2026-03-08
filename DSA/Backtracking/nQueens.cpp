c6d21a91f4e0b84e90bbf32b2a5f2e253479c93a
Divyam Agarwal <divyam7246@gmail.com>
2026-03-08 07:55:57 +0530

    changes

--- DSA/Backtracking/nQueens.cpp
+++ DSA/Backtracking/nQueens.cpp
@@ -1,11 +1,51 @@
 #include<iostream>
 #include<vector>
-#include<string>
 using namespace std ; 
+
+void printBoard(vector<vector<char>> board){
+    cout << endl ;
+    int n = board.size();
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout << board[i][j] << " ";
+        }
+        cout << endl ;
+    }
+    cout << "\n----------------\n\n";
+}
+
+bool isSafe(vector<vector<char>> board, int row, int j){
+    
+}
+
+void nQueens(vector<vector<char>> board, int row){
+    int n = board.size();
+    if(row == n){
+        printBoard(board);
+        return ;
+    }
+
+    for(int j=0; j<n; j++){
+        if(isSafe(board,row,j)){
+            board[row][j] = 'Q';
+            nQueens(board, row+1);
+            board[row][j] = '.';
+        } 
+    }
+
+}
 
 int main()
 {
     vector<vector<char>> board ;
-
+    int n = 2 ;
+    for(int i=0; i<n; i++){
+        vector<char> newRow ;
+        for(int j=0; j<n; j++){
+            newRow.push_back('.');
+        }
+        board.push_back(newRow);
+    }
+    nQueens(board,0);
     return 0 ; 
 }
