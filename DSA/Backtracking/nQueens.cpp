c87ac3d5493e210a7318dd3f83b026057dcd4eb5
Divyam Agarwal <divyam7246@gmail.com>
2026-03-13 03:40:24 +0530

    changes

--- DSA/Backtracking/nQueens.cpp
+++ DSA/Backtracking/nQueens.cpp
@@ -14,31 +14,57 @@ void printBoard(vector<vector<char>> board){
     cout << "\n-----------------\n\n";
 }
 
-bool isSafe(vector<vector<char>> board, int row, int j){
-    
+bool isSafe(vector<vector<char>> board, int row, int col){
+    int n = board.size();
+    // Horizontal Safety
+    for(int j=0; j<n; j++){
+        if(board[row][j] == 'Q'){
+            return false ;
+        }
+    }
+    // Vertical Safety
+    for(int i=0; i<row; i++){
+        if(board[i][col] == 'Q'){
+            return false  ;
+        }
+    }
+    // Diagonal Safety
+    // Left Diagonal
+    for(int i=row, j=col; i>=0 && j>=0 ; i--, j--){
+        if(board[i][j] == 'Q'){
+            return false ;
+        }
+    }
+    // Right Diagonal
+    for(int i=row, j=col; i>=0 && j<=n; i--,j++){
+        if(board[i][j] == 'Q'){
+            return false ;
+        }
+    }
+    return true ;
 }
 
-void nQueens(vector<vector<char>> board, int row){
+int nQueens(vector<vector<char>> board, int row){
     int n = board.size();
     if(row == n){
         printBoard(board);
-        return ;
+        return 1;
     }
-
+    int count = 0 ;
     for(int j=0; j<n; j++){
         if(isSafe(board,row,j)){
             board[row][j] = 'Q';
-            nQueens(board, row+1);
+            count += nQueens(board, row+1);
             board[row][j] = '.';
         } 
     }
-
+    return count ;
 }
 
 int main()
 {
     vector<vector<char>> board ;
-    int n = 2 ;
+    int n = 4 ;
     for(int i=0; i<n; i++){
         vector<char> newRow ;
         for(int j=0; j<n; j++){
@@ -46,6 +72,7 @@ int main()
         }
         board.push_back(newRow);
     }
-    nQueens(board,0);
+    int count = nQueens(board,0);
+    cout << "Count : " << count << endl ;
     return 0 ; 
 }
