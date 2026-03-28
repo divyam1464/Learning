7e6db31984bd89bcae763224f5ceef45a174c3c2
Divyam Agarwal <divyam7246@gmail.com>
2026-03-28 20:30:10 +0530

    changes

--- DSA/Backtracking/gridWays.cpp
+++ DSA/Backtracking/gridWays.cpp
@@ -1,18 +1,29 @@
 #include<iostream>
 using namespace std ; 
 
-int gridWays(int r, int c, int n, int m){
-    if(r==n && c==m){
-        // Destination Base Case
+int gridWays(int r, int c, int n, int m, string ans){
+    if( r == n-1 && c == m-1 ){
+        cout << ans << endl ;
+        // Destination Base Case final
         return 1 ;
     }
-    int v1 = ways(r,c+1,n,m);
-    int v2 = ways(r+1, c, n, m);
+    if( r >= n || c >= m ){
+        return 0 ;
+    }
+    // Right turn
+    int v1 = gridWays(r,c+1,n,m, ans+'R');
+
+    // Down turn
+    int v2 = gridWays(r+1, c, n, m, ans+'D');
     return v1 + v2 ;
 }
 
 int main()
 {
+    int n = 3 ;
+    int m = 3 ;
+    string ans = "";
+    cout << gridWays(0, 0, n, m, ans) << endl ;
     
     return 0 ; 
 }
