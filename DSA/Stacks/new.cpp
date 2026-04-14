e5ceb1d1dfe28857bc5e3e47837b10efe81aafdd
Divyam Agarwal <divyam7246@gmail.com>
2026-04-14 19:18:38 +0530

    changes

---
+++ DSA/Stacks/new.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+using namespace std ; 
+
+class Stack {
+    public: 
+        vector<int> vec ;
+        void push(int val){                // O(1) -> constant operations
+            vec.push_back(val);
+        }
+        void pop(){                        // O(1) -> constant operations
+            if(isEmpty()){
+                cout << "Stack is Empty\n" ; 
+                return ; 
+            }     
+            vec.pop_back();
+        }
+        int top(){                         // O(1) -> constant operations
+            if(isEmpty()){
+                cout << "Stack is Empty\n" ; 
+                return -1; 
+            }                      
+            int lastIdx = vec.size() - 1 ;
+            return vec[lastIdx] ;
+        }
+        bool isEmpty(){                    // O(1) -> constant operations
+            return vec.size() == 0 ;
+        }
+};
+
+
+int main()
+{
+    Stack s ;
+    s.push(3);
+    s.push(2);
+    s.push(1);
+    s.push(0);
+
+    while(!s.isEmpty()){
+        cout << s.top() << endl ;
+        s.pop();
+    }
+    
+    return 0 ; 
+}
