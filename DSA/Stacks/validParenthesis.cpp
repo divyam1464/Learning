ace185d6c59288e6f28bd9eb0b6cea62682e19c8
Divyam Agarwal <divyam7246@gmail.com>
2026-04-18 18:01:04 +0530

    changes

---
+++ DSA/Stacks/validParenthesis.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std ; 
+
+bool isValid(string str){
+    stack<char> s ;
+    for(int i=0; i<str.size(); i++){
+        char ch = str[i] ;
+        if( ch == '(' || ch == '[' || ch == '{'){
+            s.push(ch);
+        }
+        else{
+            if(s.empty()){
+                return false ;
+            }
+            else{
+                int top = s.top() ;
+                if( (top == '(' && ch == ')') ||
+                    (top == '[' && ch == ']') || 
+                    (top == '{' && ch == '}') ){
+                        s.pop();
+                    }
+                else{
+                    return false ;
+                }
+            }
+        }
+    }
+    return s.empty() ;
+}
+
+int main()
+{ 
+    string s1 = "([}])" ;     // Invalid one 
+    string s2 = "([{}])" ;    // Valid one 
+
+    cout << isValid(s1) << endl ;
+    cout << isValid(s2) << endl ;
+    
+    return 0 ; 
+}
