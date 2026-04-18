f6afbbfbdd8584ad981d8f414c5e3fc27e771e6d
Divyam Agarwal <divyam7246@gmail.com>
2026-04-18 23:47:15 +0530

    changes

--- DSA/Stacks/validParenthesis.cpp
+++ DSA/Stacks/validParenthesis.cpp
@@ -30,13 +30,37 @@ bool isValid(string str){
     return s.empty() ;
 }
 
+bool isDuplicate(string str){
+    stack<char> s ;
+    for(int i = 0 ; i < str.size(); i++){
+        char ch = str[i] ;
+        if ( ch != ')' ){ // non - closing elements are present then push into the stack everything
+            s.push(ch) ;
+        }
+        else{ // closing case when we encounter the closing brackets
+            if(s.top() == '('){
+                return true ; /// Duplicate condition
+            }
+            while(s.top() != '('){
+                s.pop();
+            }
+            s.pop();
+        }
+    }
+    return false ;
+}
+
 int main()
 { 
-    string s1 = "([}])" ;     // Invalid one 
-    string s2 = "([{}])" ;    // Valid one 
+    string s1 = "([}])" ;               // Invalid one 
+    string s2 = "([{}])" ;              // Valid one 
+    string s3 = "((a+b))" ;             // Valid one 
+    string s4 = "((a+b)+(c+d))" ;     // Valid one 
 
-    cout << isValid(s1) << endl ;
-    cout << isValid(s2) << endl ;
+    // cout << isValid(s1) << endl ;
+    // cout << isValid(s2) << endl ;
+    cout << isDuplicate(s3) << endl ;
+    cout << isDuplicate(s4) << endl ;
     
     return 0 ; 
 }
